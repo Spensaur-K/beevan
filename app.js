b9fb4d28dd8e0001e6d8312637e90c0c4022195d
Spencer Killen <sjkillen@ualberta.ca>
2019-04-30 15:10:24 -0600

    initial

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const body = require("body-parser");
+const dotenv = require("dotenv");
+const axios = require("axios");
+const assert = require("assert")
+
+dotenv.config();
+
+assert(process.env.PORT);
+assert(process.env.WEBHOOK);
+
+const app = express();
+
+app.use(body.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "./client")));
+
+app.get("/speak", ({ query: { message } }, res, next) => {
+    axios.post(process.env.WEBHOOK, {
+        content: message,
+    }).then(() => res.redirect("/"));
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`EXPRESS LISTENTING ON PORT ${process.env.PORT}`);
+});
+
